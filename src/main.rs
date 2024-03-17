a15278ce1088f5665f739505e158f8cd3cfc4bda
Ben Cochrane <ben.cochrane2112@gmail.com>
2024-03-17 19:29:37 -0400

    Write images based on spectrum

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use clap::{arg, command, Parser};
+use std::cmp::min;
 use std::fs::File;
 
-use symphonia::core::audio::SampleBuffer;
-use symphonia::core::codecs::DecoderOptions;
-use symphonia::core::errors::Error;
-use symphonia::core::formats::FormatOptions;
-use symphonia::core::io::MediaSourceStream;
-use symphonia::core::meta::MetadataOptions;
-use symphonia::core::probe::Hint;
+use symphonia::core::{
+    audio::SampleBuffer, codecs::DecoderOptions, errors::Error, formats::FormatOptions,
+    io::MediaSourceStream, meta::MetadataOptions, probe::Hint,
+};
+
+use spectrum_analyzer::scaling::{scale_20_times_log10, scale_to_zero_to_one};
+use spectrum_analyzer::{samples_fft_to_spectrum, FrequencyLimit};
+
+use image::{ImageBuffer, Rgba};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -18,7 +21,7 @@ struct FftResampler {
 
     /// Width of the output image
     /// Controls the number of frequency bins
-    #[arg(short, long, default_value = "64")]
+    #[arg(short, long, default_value = "32")]
     width: u32,
 
     /// Speed of the input audio
@@ -26,40 +29,26 @@ struct FftResampler {
     bpm: f32,
 }
 
-fn main() {
-    let cli = FftResampler::parse();
-
-    let file = Box::new(File::open(cli.file).unwrap());
-
-    // Create the media source stream using the boxed media source from above.
+fn extract_samples(file: Box<File>) -> Vec<f32> {
     let mss = MediaSourceStream::new(file, Default::default());
 
-    // Create a hint to help the format registry guess what format reader is appropriate. In this
-    // example we'll leave it empty.
     let hint = Hint::new();
-
-    // Use the default options when reading and decoding.
     let format_opts: FormatOptions = Default::default();
     let metadata_opts: MetadataOptions = Default::default();
     let decoder_opts: DecoderOptions = Default::default();
 
-    // Probe the media source stream for a format.
     let probed = symphonia::default::get_probe()
         .format(&hint, mss, &format_opts, &metadata_opts)
         .unwrap();
 
-    // Get the format reader yielded by the probe operation.
     let mut format = probed.format;
 
-    // Get the default track.
     let track = format.default_track().unwrap();
 
-    // Create a decoder for the track.
     let mut decoder = symphonia::default::get_codecs()
         .make(&track.codec_params, &decoder_opts)
         .unwrap();
 
-    // Store the track identifier, we'll use it to filter packets.
     let track_id = track.id;
 
     let mut sample_count = 0;
@@ -67,41 +56,23 @@ fn main() {
     let mut all_samples = Vec::new();
 
     while let Ok(packet) = format.next_packet() {
-        // Get the next packet from the format reader.
-        // If the packet does not belong to the selected track, skip it.
         if packet.track_id() != track_id {
             continue;
         }
 
-        // Decode the packet into audio samples, ignoring any decode errors.
         match decoder.decode(&packet) {
             Ok(audio_buf) => {
-                // The decoded audio samples may now be accessed via the audio buffer if per-channel
-                // slices of samples in their native decoded format is desired. Use-cases where
-                // the samples need to be accessed in an interleaved order or converted into
-                // another sample format, or a byte buffer is required, are covered by copying the
-                // audio buffer into a sample buffer or raw sample buffer, respectively. In the
-                // example below, we will copy the audio buffer into a sample buffer in an
-                // interleaved order while also converting to a f32 sample format.
-
-                // If this is the *first* decoded packet, create a sample buffer matching the
-                // decoded audio buffer format.
                 if sample_buf.is_none() {
-                    // Get the audio buffer specification.
                     let spec = *audio_buf.spec();
 
-                    // Get the capacity of the decoded buffer. Note: This is capacity, not length!
                     let duration = audio_buf.capacity() as u64;
 
-                    // Create the f32 sample buffer.
                     sample_buf = Some(SampleBuffer::<f32>::new(duration, spec));
                 }
 
-                // Copy the decoded audio buffer into the sample buffer in an interleaved format.
                 if let Some(buf) = &mut sample_buf {
                     buf.copy_interleaved_ref(audio_buf);
 
-                    // The samples may now be access via the `samples()` function.
                     sample_count += buf.samples().len();
                     all_samples.extend_from_slice(buf.samples());
                     print!("\rDecoded {} samples", sample_count);
@@ -111,5 +82,52 @@ fn main() {
             Err(_) => break,
         }
     }
-    println!("\nFinished, with {} samples", all_samples.len());
+    return all_samples;
+}
+
+const SAMPLING_RATE: u32 = 44_100;
+const SAMPLING_WINDOW: usize = 2048;
+
+fn main() {
+    let cli = FftResampler::parse();
+
+    let file = Box::new(File::open(&cli.file).unwrap());
+    let audio_samples = extract_samples(file);
+    let sample_count = audio_samples.len();
+
+    println!("\nFinished, with {} samples", audio_samples.len());
+    let w = sample_count / SAMPLING_WINDOW;
+    let h = cli.width;
+
+    let mut img = ImageBuffer::new(w as u32, h / 8);
+
+    for x in 0..w {
+        print!("Processing column {} of {}\r", x, w);
+        let sample_end = min((x + 1) * SAMPLING_WINDOW, sample_count);
+        let sample_start = x * SAMPLING_WINDOW;
+        let freqs = samples_fft_to_spectrum(
+            &audio_samples[sample_start..sample_end],
+            SAMPLING_RATE,
+            FrequencyLimit::Range(0.0, 10_000.0),
+            Some(&scale_to_zero_to_one),
+        )
+        .unwrap();
+
+        let data = freqs.data();
+        let data_size = data.len();
+
+        for y in (0..min(h as usize / 2, data_size)).step_by(4) {
+            let mut pixel = [0u8; 4]; // Initialize pixel with 4 sub-pixel components
+            for i in 0..4usize {
+                if y + i < data_size {
+                    let val = freqs.data()[y + i].1.val();
+                    pixel[i] = (val * 255.0) as u8;
+                }
+            }
+            img.put_pixel(x as u32, (y as u32) / 4, Rgba(pixel));
+        }
+    }
+
+    println!("\nSaving image...");
+    img.save(format!("{}.png", cli.file)).unwrap();
 }
