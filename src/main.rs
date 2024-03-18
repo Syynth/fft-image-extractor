2ca55fffaa9bfe674046af1ced38f4e8c2877b15
Ben Cochrane <ben.cochrane2112@gmail.com>
2024-03-17 20:04:59 -0400

    Rewrite image generator to be more useful

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ use symphonia::core::{
     io::MediaSourceStream, meta::MetadataOptions, probe::Hint,
 };
 
-use spectrum_analyzer::scaling::{scale_20_times_log10, scale_to_zero_to_one};
-use spectrum_analyzer::{samples_fft_to_spectrum, FrequencyLimit};
-
 use image::{ImageBuffer, Rgba};
+use spectrum_analyzer::scaling::scale_to_zero_to_one;
+use spectrum_analyzer::{samples_fft_to_spectrum, FrequencyLimit};
+use std::f32::consts::LN_10;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -21,12 +21,8 @@ struct FftResampler {
 
     /// Width of the output image
     /// Controls the number of frequency bins
-    #[arg(short, long, default_value = "32")]
+    #[arg(short, long, default_value = "128")]
     width: u32,
-
-    /// Speed of the input audio
-    #[arg(short, long, default_value = "160")]
-    bpm: f32,
 }
 
 fn extract_samples(file: Box<File>) -> Vec<f32> {
@@ -82,11 +78,28 @@ fn extract_samples(file: Box<File>) -> Vec<f32> {
             Err(_) => break,
         }
     }
-    return all_samples;
+    all_samples
 }
 
 const SAMPLING_RATE: u32 = 44_100;
 const SAMPLING_WINDOW: usize = 2048;
+const FREQUENCY_MAX: f32 = 10_000.0;
+
+fn nearest_power_of_two_below(x: u32) -> u32 {
+    let mut n = 1;
+    while n * 2 < x {
+        n *= 2;
+    }
+    n
+}
+
+fn nearest_power_of_two_above(x: u32) -> u32 {
+    let mut n = 1;
+    while n < x {
+        n *= 2;
+    }
+    n
+}
 
 fn main() {
     let cli = FftResampler::parse();
@@ -96,19 +109,33 @@ fn main() {
     let sample_count = audio_samples.len();
 
     println!("\nFinished, with {} samples", audio_samples.len());
-    let w = sample_count / SAMPLING_WINDOW;
+    let total_width = sample_count / SAMPLING_WINDOW;
     let h = cli.width;
 
-    let mut img = ImageBuffer::new(w as u32, h / 8);
+    // Find the nearest power of two to the total width
+    let nearest_w = nearest_power_of_two_below(total_width as u32);
+    let w: usize = nearest_w as usize / 2; // width of a single row
 
-    for x in 0..w {
-        print!("Processing column {} of {}\r", x, w);
-        let sample_end = min((x + 1) * SAMPLING_WINDOW, sample_count);
-        let sample_start = x * SAMPLING_WINDOW;
+    let img_row_height = h;
+    let row_count: u32 = (total_width / w) as u32 + 1;
+
+    let img_height = img_row_height * row_count;
+
+    let mut img = ImageBuffer::new(w as u32, nearest_power_of_two_above(img_height));
+
+    let freq_min: f32 = 20.0; // Minimum frequency (Hz)
+
+    let log_freq_min = freq_min.ln() / LN_10;
+    let log_freq_max = FREQUENCY_MAX.ln() / LN_10;
+
+    for sampling_x in 0..total_width {
+        print!("Processing column {} of {}\r", sampling_x, total_width);
+        let sample_end = min((sampling_x + 1) * SAMPLING_WINDOW, sample_count);
+        let sample_start = sampling_x * SAMPLING_WINDOW;
         let freqs = samples_fft_to_spectrum(
             &audio_samples[sample_start..sample_end],
             SAMPLING_RATE,
-            FrequencyLimit::Range(0.0, 10_000.0),
+            FrequencyLimit::Range(0.0, FREQUENCY_MAX),
             Some(&scale_to_zero_to_one),
         )
         .unwrap();
@@ -116,18 +143,43 @@ fn main() {
         let data = freqs.data();
         let data_size = data.len();
 
-        for y in (0..min(h as usize / 2, data_size)).step_by(4) {
-            let mut pixel = [0u8; 4]; // Initialize pixel with 4 sub-pixel components
-            for i in 0..4usize {
-                if y + i < data_size {
-                    let val = freqs.data()[y + i].1.val();
-                    pixel[i] = (val * 255.0) as u8;
-                }
+        let mut prev_val: u8 = 0;
+
+        let img_x = (sampling_x % w) as u32;
+        let img_row_offset = (sampling_x / w) as u32 * img_row_height;
+        let mut img_row = img_row_offset;
+
+        for sampling_y in 0..data_size {
+            let freq = freqs.data()[sampling_y].0.val();
+            let log_freq = freq.ln() / LN_10;
+
+            if log_freq < log_freq_min || log_freq > log_freq_max {
+                continue;
             }
-            img.put_pixel(x as u32, (y as u32) / 4, Rgba(pixel));
+
+            let img_row_target: u32 = ((log_freq - log_freq_min) / (log_freq_max - log_freq_min)
+                * img_row_height as f32)
+                .round() as u32
+                + img_row_offset;
+
+            while img_row < img_row_target {
+                let pixel = [prev_val; 4];
+                img.put_pixel(img_x, img_row, Rgba(pixel));
+                img_row += 1;
+            }
+
+            let val = freqs.data()[sampling_y].1.val();
+            prev_val = (val * 255.0) as u8;
+        }
+
+        while img_row < img_height {
+            let pixel = [prev_val; 4];
+            img.put_pixel(img_x, img_row, Rgba(pixel));
+            img_row += 1;
         }
     }
 
-    println!("\nSaving image...");
-    img.save(format!("{}.png", cli.file)).unwrap();
+    let img_name = format!("{}.png", cli.file);
+    println!("\nSaving image as {img_name:?} ...");
+    img.save(img_name).unwrap();
 }
